2077ae1a864039d8db61df1a759b4429cc5bd0d8
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-08-14 22:07:00 +0700

    Update HardwareController.php

--- app/Http/Controllers/HardwareController.php
+++ app/Http/Controllers/HardwareController.php
@@ -118,7 +118,7 @@ class HardwareController extends Controller
 
                 $allowedIps = DB::table('hardware_permissions')
                     ->where('user_name', $user->username)
-                    ->where('permissions_name', 'hardware.get')
+                    ->where('permissions_name', 'xem phần cứng')
                     ->pluck('hardware_ip');
 
                 Log::info('Found allowed IPs for user', ['username' => $user->username, 'allowedIps' => $allowedIps->toArray()]);
